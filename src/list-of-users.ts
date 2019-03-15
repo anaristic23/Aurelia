a74b087eee8e77da1c3359f6b7ca624396e72f0e
Ana Ristic <risticana91@gmail.com>
2019-03-15 17:20:40 +0100

    list of users wp

---
+++ src/list-of-users.ts
@@ -0,0 +1,12 @@
+export class ListOfUsers {
+  public heading: string =  'List Of Users';
+  public name: string = "";
+  public surname: string = "";
+  public email: string = "";
+  public usersLoc: any;
+  public rememberMe: false;
+  
+  getUsers(){
+
+  }
+}
